09254d80eeab4ad0498780f3fb68303b9b602cd8
Eduard Cristian Tasca <eduardcristiantasca@gmail.com>
2021-10-24 22:23:59 +0300

    ex3 - refactor

--- ex3/src/App.js
+++ ex3/src/App.js
@@ -4,20 +4,15 @@ import OrangeCounter from './components/OrangeCounter';
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const [oranges, setOranges] = useState([]);
 
   const moreOranges = () => {
-    let orangesClone = oranges;
-    
     setCounter(counter + 1);
-    orangesClone.push('🍊');
-    setOranges([...orangesClone])
   }
 
   return (
     <div className="App">
       <button onClick={moreOranges}>Add oranges</button>
-      <OrangeCounter oranges={oranges} />
+      <OrangeCounter counter={counter} />
     </div>
   );
 }
--- ex3/src/components/OrangeCounter.js
+++ ex3/src/components/OrangeCounter.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
 const OrangeCounter = (props) => {
+    const renderOranges = () => {
+        const counter = props.counter;
+        let oranges = [];
+
+        for(let i = 0; i < counter; i++) {
+          oranges.push('🍊');
+        }
+
+        return oranges.map(orange => orange);
+      }
+
     return (
         <div>
-            {props.oranges}
+            {renderOranges()}
         </div>
     )
 }
