63ac55cb1846be0487b057e62c849970a0a67b1b
Eduard Cristian Tasca <eduardcristiantasca@gmail.com>
2021-10-26 00:56:09 +0300

    ex4

--- ex4/src/App.js
+++ ex4/src/App.js
@@ -1,29 +1,39 @@
 import React from 'react';
 import './App.css';
 
-
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.timeoutId = null;
     this.state = {
       boxes: [
-          [
-              '🍊','🍊','🍋','🍌','🍋','🍌','🍋','🍊','🍌','🍌'
-          ],
-          [
-              '🍋','🍋','🍊','🍌','🍊','🍌','🍋','🍋','🍊','🍊','🍌',
-          ],
-          [
-              '🍌','🍋','🍌','🍋','🍊','🍌','🍌',
-          ]
+        [
+          '🍊','🍊','🍋','🍌','🍋','🍌','🍋','🍊','🍌','🍌'
+        ],
+        [
+          '🍋','🍋','🍊','🍌','🍊','🍌','🍋','🍋','🍊','🍊','🍌',
+        ],
+        [
+          '🍌','🍋','🍌','🍋','🍊','🍌','🍌',
         ]
-    }
+      ],
+      sortBy: 'default'
+    };
   }
 
   renderBoxes = () => {
-    const boxes = this.state.boxes;
+    let boxes = this.state.boxes;
+
+    if(this.state.sortBy === 'fruits') {
+      boxes = [...this.getSortedFruits(['🍊', '🍌', '🍋'])];
+    } else if(this.state.sortBy === 'boxes') {
+      boxes = [...this.getSortedBoxes()];
+    } else if(this.state.sortBy === 'random') {
+      boxes = this.getShuffledBoxes(boxes);
+    }
 
     return boxes.map((box, index) => {
+      // maybe it's not always ideal to use index as key (?)
       return (
         <div key={index} className="box">
             {
@@ -38,9 +48,8 @@ class App extends React.Component {
     })
   }
 
-  SortFruits = () => {
+  getSortedFruits = (fruitOrder) => {
     const boxes = this.state.boxes;
-    const fruitOrder = ['🍊', '🍌', '🍋'];
     const sortedFruits = boxes.map(box => {
       return box.sort((a, b) => {
         return fruitOrder.indexOf(a) - fruitOrder.indexOf(b)
@@ -50,8 +59,8 @@ class App extends React.Component {
     return sortedFruits;
   }
 
-  SortBoxes = () => {
-    const allFruits = [].concat(...this.state.boxes);
+  getSortedBoxes = () => {
+    const allFruits = [].concat(...this.getSortedFruits(['🍊', '🍌', '🍋']));
     const typesOfFruits = this.getTypesOfFruits(allFruits);
     let sameFruitBoxes = new Array(typesOfFruits.length).fill([])
 
@@ -66,9 +75,20 @@ class App extends React.Component {
     return [...sameFruitBoxes];
   }
 
-  randomizeBoxes = () => {
-    const allFruits = [].concat(...this.state.boxes);
+  getRandomElements(elem) {
+    for (let i = elem.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [elem[i], elem[j]] = [elem[j], elem[i]];
+    }
 
+    return elem;
+  }
+
+  getShuffledBoxes(boxes) {
+    boxes = this.getRandomElements(boxes);
+    boxes = boxes.map(box => this.getRandomElements(box));
+
+    return boxes;
   }
 
   getTypesOfFruits = (fruits) => {
@@ -77,11 +97,36 @@ class App extends React.Component {
     return [...typesOfFruits];
   }
 
+  setSortBy = (e) => {
+    this.setState({
+      sortBy: e.target.value
+    }, () => {
+      this.timeoutId = this.customTimeout();
+    })
+  }
+
+  customTimeout = () => {
+    return setTimeout(() => {
+      this.setState({
+        sortBy: 'random'
+      })
+    }, 3000)
+  }
+
+  componentDidUpdate() {
+    clearTimeout(this.timeoutId);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeoutId);
+  }
+
   render() {
     return (
       <div className="App">
-        <button className="button" type="button" onClick={this.SortFruits}>Sort Fruits</button>
-        <button className="button" type="button" onClick={this.SortBoxes}>Sort Boxes</button>
+        <button className="button" type="button" value="fruits" onClick={this.setSortBy}>Sort Fruits</button>
+        <button className="button" type="button" value="boxes" onClick={this.setSortBy}>Sort Boxes</button>
+        <button className="button" type="button" value="random" onClick={this.setSortBy}>Randomize</button>
         {this.renderBoxes()}
       </div>
     );
